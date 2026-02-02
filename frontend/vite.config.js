fc6bdafb903ff104d0e98ee8e8da669da709ab68
dylanpavelko <dpavelko@gmail.com>
2026-02-01 19:40:51 -0800

    updated vite config to help with console error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/the-full-price-frameworks/' : '/',
+  base: '/the_full_price_frameworks/',
   plugins: [react()],
   server: {
     port: 3000,
